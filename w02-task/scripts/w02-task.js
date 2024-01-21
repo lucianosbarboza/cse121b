0aa8a5643746809fa91345f63626f75181d8c470
lucianosbarboza <lucianosbarboza@hotmail.com>
2024-01-21 13:04:23 -0300

    completion of exercises

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,25 +3,39 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+let fullName = 'Luciano S Barboza';
+let currentDate = new Date().getFullYear();
+const profilePicture = 'images/placeholder.png';
+let favoriteFoods = ['Arabic food', 'Italian food', 'Brazilian food'];
 
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('profileImage');
 
 
 
 
 /* Step 4 - Adding Content */
 
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentDate;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 
+// Set the initial content of foodElement
+foodElement.innerHTML = favoriteFoods.join('<br>');
+let newFavoriteFood = 'Ice Cream';
+favoriteFoods.push(newFavoriteFood);
+foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods.join('<br>')}`;
 
 
 
